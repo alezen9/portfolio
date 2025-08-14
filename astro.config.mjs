7724d1e7aab6cc930ca56b519b04780cb64279c4
Aleksandar Gjoreski <aleksandar.d.gjoreski@gmail.com>
2025-08-14 11:58:31 +0200

    chore: update to custom dns aleksandargjoreski.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alezen9.github.io",
-  base: "portfolio",
+  site: "https://aleksandargjoreski.dev",
+  base: "/",
   trailingSlash: "ignore",
   integrations: [mdx(), sitemap(), preact()],
   devToolbar: {
