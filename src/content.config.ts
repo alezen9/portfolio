a9045a17c188491fe6a94d566460f3624e5e9b0e
Aleksandar Gjoreski <aleksandar.d.gjoreski@gmail.com>
2025-08-26 00:56:55 +0200

    feat: draft some possible blog posts, no content just titles + minor progress in blog page

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,26 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+
+const cleanupBlogEntry = (opts: { entry: string }) => {
+  const withoutIdx = opts.entry.replace(/^\d{3}-/, "");
+  const withoutExt = withoutIdx.split(".")[0];
+  return withoutExt;
+};
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: image().optional(),
-	}),
+  loader: glob({
+    base: "./src/content/blog",
+    pattern: "**/*.{md,mdx}",
+    generateId: cleanupBlogEntry,
+  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(),
+    }),
 });
 
 export const collections = { blog };
